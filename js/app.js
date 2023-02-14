1e43e20723a8abac252c012ccfc32dcff86e5bf3
kimyoonjii <kimuj0823@naver.com>
2023-02-14 09:44:48 +0900

    로그인페이지링크테스트완료 a링크 스크립트 추가작업

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 // JavaScript Document
 'use strict';
 
+window.addEventListener('load', function() {
+    preventDefault();
+  
+  }, false);
+  
+  
+  function preventDefault() {
+    const links = document.querySelectorAll('a[href="#"]');
+    links.forEach(function(element) {
+      element.addEventListener('click', function(e) {
+        e.preventDefault();
+      }, false);
+    });
+  }
+
 $(document).ready(function() {
     // Sidebar menu
     setSideMenu();
